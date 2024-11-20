72fd4b9706f8085fd6f52d586fb13b5df12b0324
Sklepinis <pauliussurelis@gmail.com>
2024-11-21 01:04:06 +0200

    update

--- obj/Debug/net8.0/Kazkas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Kazkas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kazkas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e8352bfb3ec74bf3ae7c4fd4cef863cfc56f09f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kazkas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kazkas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
